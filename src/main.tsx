ac23fdfb266cc8a2aca1896c44afac7b7ddebe5e
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-05-24 15:07:51 -0400

    Add MUI config

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { RecoilRoot } from 'recoil';
 import HomeApp from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <RecoilRoot>
+        <CssBaseline />
         <HomeApp />
       </RecoilRoot>
     </BrowserRouter>
