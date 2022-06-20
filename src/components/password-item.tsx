2a2e04b39ac00d288f13bbabc8c0e27bb1f397f1
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-06-20 18:59:29 -0400

    Add responsive password-item component

--- src/components/password-item.tsx
+++ src/components/password-item.tsx
@@ -2,6 +2,8 @@ import { Avatar, Box, ListItem, ListItemText, OutlinedInput, IconButton } from '
 import { Password } from '../types/index';
 import EnhancedEncryptionIcon from '@mui/icons-material/EnhancedEncryption';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 
 interface Props {
   password: Password;
@@ -18,29 +20,110 @@ const PasswordItem = ({ password }: Props) => {
       passwordField.type = 'password';
     }, VISIBLE_TIME);
   };
-  return (
-    <ListItem>
-      <Avatar sx={{ mr: 2, backgroundColor: 'primary.main' }}>
-        <EnhancedEncryptionIcon />
-      </Avatar>
-      <ListItemText primary={password.name} secondary={password.url} />
 
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <OutlinedInput id="standard-basic" type="text" value={password.username} sx={{ mr: 2 }} />
-        <OutlinedInput
-          id={`password-fiel-${password.id}`}
-          type="password"
-          value={password.key}
-          name="password"
-          sx={{ mr: 2 }}
-          endAdornment={
-            <IconButton aria-label="Show password" onClick={visibilityHandler}>
-              <VisibilityIcon />
-            </IconButton>
-          }
-        />
+  return (
+    <>
+      {/* Mobile */}
+      <ListItem>
+        <Box sx={{ display: { xs: 'block', md: 'none' }, mb: 4 }}>
+          <Box sx={{ display: 'flex' }}>
+            <Avatar sx={{ mr: 2, backgroundColor: 'primary.main' }}>
+              <EnhancedEncryptionIcon />
+            </Avatar>
+            <ListItemText primary={password.name} secondary={password.url} sx={{ display: 'inline' }} />
+
+            <Box sx={{ mr: 2 }}>
+              <IconButton
+                color="primary"
+                edge="end"
+                aria-label="rename"
+                onClick={() => {
+                  alert(`rename ${password.name}`);
+                }}
+              >
+                <DriveFileRenameOutlineIcon />
+              </IconButton>
+
+              <IconButton
+                color="primary"
+                edge="end"
+                aria-label="delete"
+                onClick={() => {
+                  alert(`delete ${password.name}`);
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <OutlinedInput id="standard-basic" type="text" value={password.username} sx={{ mr: 2 }} fullWidth />
+            <OutlinedInput
+              id={`password-fiel-${password.id}`}
+              type="password"
+              value={password.key}
+              name="password"
+              sx={{ mr: 2 }}
+              fullWidth
+              endAdornment={
+                <IconButton aria-label="Show password" onClick={visibilityHandler}>
+                  <VisibilityIcon />
+                </IconButton>
+              }
+            />
+          </Box>
+        </Box>
+      </ListItem>
+
+      {/* Desktop */}
+      <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+        <ListItem>
+          <Avatar sx={{ mr: 2, backgroundColor: 'primary.main' }}>
+            <EnhancedEncryptionIcon />
+          </Avatar>
+          <ListItemText primary={password.name} secondary={password.url} />
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <OutlinedInput id="standard-basic" type="text" value={password.username} sx={{ mr: 2 }} />
+            <OutlinedInput
+              id={`password-fiel-${password.id}`}
+              type="password"
+              value={password.key}
+              name="password"
+              sx={{ mr: 2 }}
+              endAdornment={
+                <IconButton aria-label="Show password" onClick={visibilityHandler}>
+                  <VisibilityIcon />
+                </IconButton>
+              }
+            />
+            <Box sx={{ mr: 2 }}>
+              <IconButton
+                color="primary"
+                edge="end"
+                aria-label="rename"
+                onClick={() => {
+                  alert(`rename ${password.name}`);
+                }}
+              >
+                <DriveFileRenameOutlineIcon />
+              </IconButton>
+
+              <IconButton
+                color="primary"
+                edge="end"
+                aria-label="delete"
+                onClick={() => {
+                  alert(`delete ${password.name}`);
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </ListItem>
       </Box>
-    </ListItem>
+    </>
   );
 };
 
