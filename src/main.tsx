d750db54dbb3e51bd16a2675be02cafb878dd3e7
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-06-21 14:20:35 -0400

    Fix document.body error

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
 
-ReactDOM.createRoot(document.body!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <RecoilRoot>
