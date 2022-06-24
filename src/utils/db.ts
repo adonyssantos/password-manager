511bee60c5ebbad9ee04ba7f050691bd4d8016a5
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-06-24 12:07:41 -0400

    Add db utilities file

---
+++ src/utils/db.ts
@@ -0,0 +1,13 @@
+import { db } from '../services';
+
+// TODO: Create document in collection
+
+// TODO: Get all documents from collection
+
+// TODO: Get document from collection
+
+// TODO: Get document from collection by id
+
+// TODO: Update document in collection by id
+
+// TODO: Delete document from collection by id
