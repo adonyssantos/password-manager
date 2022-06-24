3d8062d5fe97708f37dc5960b9ab319745a84db6
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-06-24 12:17:15 -0400

    Add createDocument utility

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,20 @@
 import { db } from '../services';
+import { collection, addDoc } from 'firebase/firestore';
 
-// TODO: Create document in collection
+/**
+ * It creates a document in a collection
+ * @param {string} collectionPath - The path to the collection you want to create a document in.
+ * @param {T} documentData - The data you want to add to the document.
+ * @returns A promise that resolves to a string.
+ */
+export const createDocument = async <T>(collectionPath: string, documentData: T): Promise<string> => {
+  try {
+    const docRef = await addDoc(collection(db, collectionPath), documentData);
+    return Promise.resolve(`Document ${docRef.id} created successfully.`);
+  } catch (error) {
+    return Promise.reject(`Error creating document: ${error}`);
+  }
+};
 
 // TODO: Get all documents from collection
 
