e985182d79224244c5e9bdc64d571ab98046c7c0
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-06-24 12:55:17 -0400

    Add getDocumentsByField utility

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,5 +1,5 @@
 import { db } from '../services';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, query, where, addDoc, getDocs } from 'firebase/firestore';
 
 /**
  * It creates a document in a collection
@@ -22,9 +22,9 @@ export const createDocument = async <T>(collectionPath: string, documentData: T)
  * @param {string} collectionPath - The path to the collection you want to get documents from.
  * @returns An array of objects.
  */
-export const getDocuments = async (collectionPath: string): Promise<unknown[] | string> => {
+export const getDocuments = async (collectionPath: string): Promise<object[] | string> => {
   try {
-    const data: unknown[] = [];
+    const data: object[] = [];
     const querySnapshot = await getDocs(collection(db, collectionPath));
 
     querySnapshot.forEach((doc) => {
@@ -40,9 +40,35 @@ export const getDocuments = async (collectionPath: string): Promise<unknown[] | 
   }
 };
 
-// TODO: Get document from collection
+/**
+ * Get all documents from a collection where a field equals a value
+ * @param {string} collectionPath - The path to the collection you want to query.
+ * @param {string} documentField - The field in the document that you want to query by.
+ * @param {string} fieldValue - The value of the field you want to query by.
+ * @returns An array of objects.
+ */
+export const getDocumentsByField = async (
+  collectionPath: string,
+  documentField: string,
+  fieldValue: string
+): Promise<object[] | string> => {
+  try {
+    const data: object[] = [];
+    const q = query(collection(db, collectionPath), where(documentField, '==', fieldValue));
+    const querySnapshot = await getDocs(q);
 
-// TODO: Get document from collection by id
+    querySnapshot.forEach((doc) => {
+      data.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    return Promise.resolve(data);
+  } catch (error) {
+    return Promise.reject(`Error getting documents: ${error}`);
+  }
+};
 
 // TODO: Update document in collection by id
 
