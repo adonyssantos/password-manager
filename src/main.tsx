4dee150cd2f37cbf32af7c3cc2db194c6a617e12
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-07-11 15:17:12 -0400

    Update some files

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { RecoilRoot } from 'recoil';
 import HomeApp from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssBaseline } from '@mui/material';
 import { HelmetProvider } from 'react-helmet-async';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/utils/folders.ts
+++ src/utils/folders.ts
@@ -1,6 +1,6 @@
 // TODO: Add folder
 
-// TODO: Get an array of all folders in the database
+// TODO: Get an array of all folders in the database with the qty of passwords in this folder
 
 // TODO: Update a folder by id
 
