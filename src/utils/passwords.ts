57bf31cf88cb6ed462a718175bea0a225e58493c
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-07-11 16:28:59 -0400

    Add passwords utilities documentation

--- src/utils/passwords.ts
+++ src/utils/passwords.ts
@@ -1,7 +1,11 @@
 import { Password, PasswordParams, UpdatePasswordParams } from '../types';
 import { createDocument, updateDocument, deleteDocument } from './db';
 
-// TODO: Add password
+/**
+ * It takes a password object and creates a new document in the passwords collection
+ * @param {PasswordParams} password - PasswordParams
+ * @returns A promise that resolves to the newly created document.
+ */
 export const addPassword = (password: PasswordParams) => {
   return createDocument('passwords', password);
 };
@@ -13,12 +17,23 @@ export const addPassword = (password: PasswordParams) => {
 //* 4. Get the user folder list
 //* 5. Group the passwords by folder and return it
 
-// TODO: Update a password by id
+/**
+ * It updates a password document in the passwords collection
+ * @param id - The id of the password you want to update.
+ * @param {UpdatePasswordParams} newData - UpdatePasswordParams
+ * @returns A function that takes an id and newData and returns a promise that resolves to the updated
+ * document.
+ */
 export const updatePassword = (id: Password['id'], newData: UpdatePasswordParams) => {
   return updateDocument<UpdatePasswordParams>('passwords', id, newData);
 };
 
-// TODO: Delete a password by id
+/**
+ * It deletes a password document from the passwords collection
+ * @param id - The id of the document to delete.
+ * @returns A function that takes an id and returns a promise that resolves to the result of the
+ * deleteDocument function.
+ */
 export const deletePassword = (id: Password['id']) => {
   return deleteDocument('passwords', id);
 };
