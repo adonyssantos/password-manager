f82192279cfef30d47f270e2de6c4c9adfbb9dd6
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-07-12 16:47:18 -0400

    Update Dashboard hooks

--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -1,20 +1,23 @@
 import { Box, Typography } from '@mui/material';
 import { getPasswordGroupByFolders } from '../utils';
-import { PasswordGroup, PasswordItem, PlusButton, SEO } from '../components';
-import { PasswordGroupByFolder } from '../types';
+import { PasswordGroup, PasswordItem, PlusButton, SEO, Loading } from '../components';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useUserState } from '../hooks';
+import { useUserState, usePasswordGroup } from '../hooks';
 
 const Dashboard = () => {
-  const [passwordGroupByFolders, setPasswordGroupByFolders] = useState<PasswordGroupByFolder>([]);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { user } = useUserState();
+  const { passwordGroupByName, setPasswordGroupByName } = usePasswordGroup();
 
   useEffect(() => {
     if (user) {
       const { uid, masterPassword } = user;
-      getPasswordGroupByFolders(uid, masterPassword).then(setPasswordGroupByFolders);
+
+      getPasswordGroupByFolders(uid, masterPassword)
+        .then(setPasswordGroupByName)
+        .then(() => setLoading(false));
     }
   }, []);
 
@@ -30,13 +33,17 @@ const Dashboard = () => {
           </Typography>
         </Box>
 
-        {passwordGroupByFolders.map((passwordGroupByFolder, index) => (
-          <PasswordGroup key={passwordGroupByFolder.id} name={passwordGroupByFolder.name} type={index === 0 ? 'default' : ''}>
-            {passwordGroupByFolder.passwords.map((password) => (
-              <PasswordItem key={password.id} password={password} />
-            ))}
-          </PasswordGroup>
-        ))}
+        {loading ? (
+          <Loading name="Getting passwords..." />
+        ) : (
+          passwordGroupByName?.map((passwordGroupByFolder, index) => (
+            <PasswordGroup key={passwordGroupByFolder.id} name={passwordGroupByFolder.name} type={index === 0 ? 'default' : ''}>
+              {passwordGroupByFolder.passwords.map((password) => (
+                <PasswordItem key={password.id} password={password} />
+              ))}
+            </PasswordGroup>
+          ))
+        )}
 
         <PlusButton title="Add Password" onClick={() => navigate('/passwords/add')} />
       </Box>
