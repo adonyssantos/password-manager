5c2f8d46e9b7c3376eccae93c2bc44e371c16941
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-07-12 19:13:55 -0400

    Add hook to add new folder

--- src/views/add-folder.tsx
+++ src/views/add-folder.tsx
@@ -1,12 +1,36 @@
+import { addFolder } from '../utils';
 import { Box, Button, TextField, Typography } from '@mui/material';
-import { useState, FormEvent } from 'react';
+import { PasswordsFolder } from '../types';
 import { SEO } from '../components';
+import { useNavigate } from 'react-router-dom';
+import { useUserState } from '../hooks';
+import { useState, FormEvent } from 'react';
 
 const AddFolder = () => {
   const [disableButton, setDisableButton] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const { user } = useUserState();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    alert('Test');
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const name = formData.get('name') as PasswordsFolder['name'];
+
+    event.preventDefault();
+    setDisableButton(true);
+
+    if (user && user.uid) {
+      addFolder({
+        name,
+        userId: user.uid,
+      })
+        .then(() => {
+          navigate('/folders');
+        })
+        .catch(() => {
+          setDisableButton(false);
+        });
+    }
   };
 
   return (
@@ -25,6 +49,18 @@ const AddFolder = () => {
           <TextField margin="normal" required fullWidth id="name" label="Folder name" name="name" autoFocus />
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 2 }} color="primary" disabled={disableButton}>
             Add Password
+          </Button>
+          <Button
+            type="button"
+            fullWidth
+            variant="outlined"
+            sx={{ mt: 2 }}
+            color="primary"
+            onClick={() => {
+              navigate('/folders');
+            }}
+          >
+            Cancel
           </Button>
         </Box>
       </Box>
